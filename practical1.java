be0fcd0f77d9ea851bdc8ced8f01c45c39e66ea3
MEEGODA M.N.H it23283626 <nipun.meegoda@gmail.com>
2024-07-16 13:31:37 +0530

    First Commit

---
+++ practical1.java
@@ -0,0 +1,14 @@
+package practical01;
+
+public class Welcome{
+  public static void main(String[] args){
+    String FirstName, LastName;
+    java.util.Scanner myKey = new java.util.Scanner(System.in);
+
+    System.out.print("Enter First Name");
+    FirstName = myKey.nextLine();
+    System.out.print("Enter Last Name");
+    LastName = myKey.nextLine();
+    System.out.println("Welcome to the Second Year" + FirstName + LastName);	
+  }
+}
